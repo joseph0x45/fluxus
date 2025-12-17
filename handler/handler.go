739c8c988bd8800278c4d5b534cf1a2162656fbb
joseph0x45 <zozozozeph@gmail.com>
2025-12-17 18:21:00 +0000

    bit of refactoring

--- handler/handler.go
+++ handler/handler.go
@@ -37,24 +37,3 @@ func (h *Handler) getCurrentUser(w http.ResponseWriter, r *http.Request) *models
 	}
 	return currentUser
 }
-
-func (h *Handler) RedirectToAccountsPage(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/accounts", http.StatusSeeOther)
-}
-
-func (h *Handler) RenderAccountsPage(w http.ResponseWriter, r *http.Request) {
-	currentUser := h.getCurrentUser(w, r)
-	if currentUser == nil {
-		return
-	}
-	pageData := models.PageData{
-		Title:      "Fluxus | Accounts",
-		ActivePage: "accounts",
-		Payload: map[string]any{
-			"User": currentUser,
-		},
-	}
-	if err := h.templates.ExecuteTemplate(w, "accounts", pageData); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
